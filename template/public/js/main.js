2c4c56e5709cfb95eabf5e7b1ec527427c1c3bc6
ngothanhduoc <ngothanhduoc1991@gmail.com>
2014-11-13 00:31:08 +0700

    update

--- template/public/js/main.js
+++ template/public/js/main.js
@@ -2,20 +2,39 @@ $(function () {
     //load giao dien cac vong tron
     loadCirle();
     $('.shadow').hide();
-    $('.arrow-left').click(function () {
-        $('.block').removeClass('animated zoomIn');
-        $('.block').addClass('animated flipOutX');
-        callback();
+//    $('.arrow-left').click(function () {
+//        $('.block').removeClass('animated zoomIn');
+//        $('.block').addClass('animated flipOutX');
+//        callback();
+//    });
+//    $('.arrow-right').click(function () {
+//        $('.block').removeClass('animated zoomIn');
+//        $('.block').addClass('animated flipOutX');
+//        callback();
+//    });
+
+    $('.arrow-left,.arrow-right').rotate({
+        bind:
+                {
+                    click: function () {
+                        $('.content').rotate({angle: 0, animateTo: 45, easing: $.easing.easeInOutExpo});
+                        $('.block').removeClass('animated zoomIn');
+                        $('.block').addClass('animated flipOutX');
+                        callback();
+
+                    }
+                }
+
     });
-    $('.arrow-right').click(function () {
-        $('.block').removeClass('animated zoomIn');
-        $('.block').addClass('animated flipOutX');
-        callback();
-    });
+
     $('.close').click(
             function () {
                 $('.shadow').removeClass('animated zoomIn');
                 $('.shadow').addClass('animated zoomOut');
+                
+                setTimeout(function () {
+                   $('.shadow').hide();
+                }, 1000);
             }
     )
     $('.feedback').click(function () {
@@ -25,7 +44,6 @@ $(function () {
 
     function callback() {
         setTimeout(function () {
-//            $('.content').addClass('animated zoomOut');
             loadCirle();
         }, 1000);
     }
@@ -39,7 +57,11 @@ $(function () {
             $('.content').html(response);
 //            $('.content').addClass('animated zoomIn');
             $('.block').addClass('animated zoomIn');
+
             hoverCircle();
+            $style = $('.content').attr('style');
+            if ($style != undefined)
+                $('.circle-main').addClass('deg45');
         }).fail(function () {
             alert('Có lỗi ! Không kết nối đến dữ liệu được.');
         });
@@ -48,9 +70,13 @@ $(function () {
         $('.circle').hover(
                 function () {
                     $(this).addClass('animated pulse');
+                    $style = $('.content').attr('style');
+                    if ($style != undefined)
+                        $('.circle-main').addClass('deg45');
                 },
                 function () {
                     $(this).removeClass('animated pulse');
+
                 }
         );
     }
